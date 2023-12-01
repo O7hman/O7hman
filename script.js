21638274b53db25fd24d7a4cf314e80b741b32cc
Othman <142194334+O7hman@users.noreply.github.com>
2023-12-01 10:47:34 -0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const open_link = document.querySelector('#open-journal')
-const bg = document.querySelector('.journal-bg')
+// const bg = document.querySelector('.journal-bg')
 const intro_para = document.querySelector('.intro')
 var flag = 'closed'
 open_link.addEventListener('click', (event)=>{
@@ -17,3 +17,45 @@ open_link.addEventListener('click', (event)=>{
     }
 
 })
+
+// IMAGE CREDITS
+// runyourpersonalbest.com | running
+// enhancv.com | programming
+// unsplash, Javier Martinez | gamming
+// educationdestinationmalaysia.com | language
+// pixabay.com | travel
+
+var items = ['programming', 'running', 'gamming', 'sight-seeing', 'language-learning', 'traveling']
+
+var intro = document.querySelector('.intro')
+
+items.forEach(m=>{
+    let link = document.createElement('a')
+    link.href = `${m}.html`
+    link.target = '_blank'
+
+    let wrapper = document.createElement('div')
+    wrapper.style.background = `url(images/${m}.jpeg) no-repeat center/cover`
+    wrapper.style.position = 'relative'
+
+    let back = document.createElement('div')
+    back.style.position = 'absolute'
+    back.style.right= '0'
+    back.style.bottom = '0'
+    back.style.borderLeft = '20px solid transparent'
+    back.style.borderBottom = '20px solid white'
+
+    let title = document.createElement('h1')
+    title.innerText = m
+    title.style.position = 'relative'
+    title.style.top = '28px'
+    title.style.fontWeight = 400
+    title.style.fontSize = '1rem'
+
+    link.append(title)
+    back.appendChild(link)
+    wrapper.appendChild(back)
+    intro.appendChild(wrapper)
+})
+
+
